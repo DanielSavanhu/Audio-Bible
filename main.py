e082dbc999e52ba4a9772856dd7b6ec54345b774
Daniel Savanhu <dannysavanhu@gmail.com>
2020-11-04 20:34:06 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import pyttsx3
-from tkinter import *
-import tkinter as tk
-from tkinter import ttk
-
-
-class Root(Frame):
-    def __init__(self):
-
-        tk.Frame.__init__(self)
-        self.pack()
-        self.master.title('HOLY BIBLE')
-        self.title('HOLY BIBLE')
-        self.minsize(800, 400)
-
 
 engine = pyttsx3.init()
-win = Root()
-win.mainloop()
 
 def read(search, book):
     engine = pyttsx3.init()
@@ -32,7 +16,7 @@ while True:
     search = input('Please enter verse:\n e.g Genesis 1:20, Genesis, Genesis 5\n ')
     search.lstrip().rstrip()
     scripture = search.split(' ')[0].rstrip()
-    text = str(open(scripture + '.txt', 'rt').read())
+    text = str(open('Bible/' + scripture + '.txt', 'rt').read())
     book = text.split('\n')
     read(search, book)
     print(book)
